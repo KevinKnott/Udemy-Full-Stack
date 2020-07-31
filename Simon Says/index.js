77c7dbb11bebae47a89f45ebe788eea0cca4bb11
Kevin Knott <KKnoTT93@gmail.com>
2020-07-31 16:44:08 -0500

    Finished up the logic

--- Simon Says/index.js
+++ Simon Says/index.js
@@ -3,7 +3,11 @@
 // Fail causes game over class
 // Pass causes index to move one until end
 
-
+var green = new Audio("sounds/green.mp3");
+var red = new Audio("sounds/red.mp3");
+var yellow = new Audio("sounds/yellow.mp3");
+var blue = new Audio("sounds/blue.mp3");
+var wrong = new Audio("sounds/wrong.mp3");
 
 
 // Var create game object
@@ -11,6 +15,7 @@ function Game() {
     // Initialize
     this.simonOrder = [];
     this.index = 0;
+    this.level = 0;
     this.isOver = true;
     this.colorMap = {
         "1": "green",
@@ -23,24 +28,37 @@ function Game() {
     this.addColor = function () {
         var color = Math.random() * 4;
         color = Math.floor(color) + 1;
-        this.simonOrder.push(this.colorMap[color]);
+        color = this.colorMap[color]
+
+        this.simonOrder.push(color);
+        this.level += 1;
+        $("#" + color).addClass("pressed")
+        playButtonSound(color)
+        setTimeout(function () {
+            $("#" + color).removeClass("pressed")
+        }, 200)
     }
 
     // wait for button click?
     this.checkColor = function (id) {
-        console.log(ourGame.isOver);
-        console.log(!ourGame.isOver);
         if (!ourGame.isOver) {
-            alert("Check back next time");
+            if (id === ourGame.simonOrder[this.index]) {
+                this.index++;
+
+                if (this.index === this.level) {
+                    ourGame.addColor();
+                    changeLevelTitle("Level " + this.level);
+                    this.index = 0;
+                }
+            } else {
+                gameOver();
+            }
 
         } else {
-            alert("Your Game is over");
-
+            gameOver();
         }
     };
 
-    // Add first color for when game starts
-    this.addColor();
 };
 
 var ourGame = new Game();
@@ -53,13 +71,47 @@ function startGame() {
     if (ourGame.isOver) {
         ourGame = new Game();
         ourGame.isOver = false;
-        console.log(ourGame.simonOrder + " null ");
-    } else {
-        console.log("still in our game");
+        ourGame.addColor();
+        changeLevelTitle("Level " + ourGame.level);
+        // console.log(ourGame.simonOrder + " null ");
     }
 
 }
 
+function gameOver() {
+    ourGame.isOver = true;
+    wrong.play();
+    changeLevelTitle("Game Over, Press Any Key to Restart");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+        $("body").removeClass("game-over");
+    }, 200)
+}
+
+function changeLevelTitle(msg) {
+    $("#level-title").text(msg);
+}
+
+function playButtonSound(id) {
+    switch (id) {
+        case "green":
+            green.play();
+            break;
+        case "red":
+            red.play();
+            break;
+        case "yellow":
+            yellow.play();
+            break;
+        case "blue":
+            blue.play();
+            break;
+        default:
+            console.log("No id");
+            break;
+    }
+
+}
 
 
 $(document).on("keydown", function () {
@@ -69,4 +121,6 @@ $(document).on("keydown", function () {
 $(".btn").on("click", function () {
     // console.log(this.id);
     ourGame.checkColor(this.id);
+    playButtonSound(this.id);
+
 });
