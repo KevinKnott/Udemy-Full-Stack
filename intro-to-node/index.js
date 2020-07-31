324016486685e23780e1f5ea345f2f3ffdec7625
Kevin Knott <KKnoTT93@gmail.com>
2020-07-31 18:41:50 -0500

    Simple superhero showdown app

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,3 +1,16 @@
-const fs = require('fs');
+// Internal module use
+// const fs = require('fs');
 
-fs.copyFileSync("file1.txt", "file2.txt");
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+// External module use
+// npm install <module>
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+
+var mySuperHeroName = superheroes.random();
+var myNemisis = supervillains.random();
+
+console.log("The super hero showdown of a lifetime!")
+console.log(mySuperHeroName + " vs " + myNemisis);
