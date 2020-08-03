1137c3c325b91467aaf5b304afa6203a628d2dd5
Kevin Knott <KKnoTT93@gmail.com>
2020-08-03 17:52:58 -0500

    Create git ignore to remove env files and small update to get endpoint added to new app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const https = require("https");
+require('dotenv').config();
+
 const app = express();
 
+
 app.get("/", function (req, res) {
+
+    const endpoint = "https://api.openweathermap.org/data/2.5/weather?q=Grapevine,TX,US&appid=" + process.env.weatherAPIKEY + "&units=imperial"
+    https.get(endpoint, function (weatherResponse) {
+        console.log(weatherResponse)
+    })
+
     res.sendFile(__dirname + "\\index.html");
 })
 
 app.listen(3000, function () {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000");
 })
