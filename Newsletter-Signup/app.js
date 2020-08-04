6af043bc83e6d41020c42d54100cbfb0daf8216f
Kevin Knott <KKnoTT93@gmail.com>
2020-08-04 18:19:44 -0500

    I have working singup page and post data to node backend

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,6 +4,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
+// Provides path to the static files by a relative url
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+
+    console.log(fname, lname, email);
+})
 
 app.listen(3000, function () {
     console.log("Server up and listening on port 3000")
