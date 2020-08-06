8ffcfa011d1821079eeae72142df0c500fa85254
Kevin Knott <KKnoTT93@gmail.com>
2020-08-06 18:50:41 -0500

    Adding simple EJS if statement to add control flow html template

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -12,16 +12,43 @@ app.get("/", function (req, res) {
     var today = new Date();
     var day = ""
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        // res.write("Its the freaken weekend baby");
-        day = "Weekend";
-    } else {
-        // res.write("Its a weekday --- BOOO");
-        day = "Weekday"
+    // if (today.getDay() === 6 || today.getDay() === 0) {
+    //     // res.write("Its the freaken weekend baby");
+    //     day = "Weekend";
+    // } else {
+    //     // res.write("Its a weekday --- BOOO");
+    //     day = "Weekday"
+    // }
+
+    switch (today.getDay()) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("invalid date please review", today);
+            break;
     }
 
     // res.send();
-
+    // First variable is on EJS side and second is the variable in your app.js
     res.render("list", { weekDay: day });
 })
 
