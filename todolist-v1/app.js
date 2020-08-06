ef60e9bc32e41dea211ddf4d6e25b07519b9925d
Kevin Knott <KKnoTT93@gmail.com>
2020-08-06 18:39:30 -0500

    Simple EJS template

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,8 +4,25 @@ require('dotenv').config();
 
 const app = express();
 
+// Tells app to use EJS as the vie engine
+app.set('view engine', 'ejs');
+
 app.get("/", function (req, res) {
-    res.send("Hello");
+
+    var today = new Date();
+    var day = ""
+
+    if (today.getDay() === 6 || today.getDay() === 0) {
+        // res.write("Its the freaken weekend baby");
+        day = "Weekend";
+    } else {
+        // res.write("Its a weekday --- BOOO");
+        day = "Weekday"
+    }
+
+    // res.send();
+
+    res.render("list", { weekDay: day });
 })
 
 
