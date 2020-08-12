e5c903f23c054307b2993b03f43d475eeba85f28
Kevin Knott <KKnoTT93@gmail.com>
2020-08-12 16:40:43 -0500

    Add mongoose connection

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -3,26 +3,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose")
+require('dotenv').config();
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true })
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
-const day = date.getDate();
+  const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", { listTitle: day, newListItems: items });
 
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
   const item = req.body.newItem;
 
@@ -35,14 +40,14 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
