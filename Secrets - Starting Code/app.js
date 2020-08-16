9dd8957a1aaba066a2a0c172888b38b817177c83
Kevin Knott <KKnoTT93@gmail.com>
2020-08-16 18:33:19 -0500

    Made basic App.js

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,1 +1,15 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require('body-parser');
+const ejs = require("ejs");
+require("dotenv").config();
+
+const app = express()
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started");
+})
