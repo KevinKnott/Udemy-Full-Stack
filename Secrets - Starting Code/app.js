28c7f3480d2596c7516fb2fd4dc2075cc7ad16f2
Kevin Knott <KKnoTT93@gmail.com>
2020-08-16 18:35:59 -0500

    Added routes to home login and register

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
+const mongoose = require('mongoose');
+const { findSeries } = require("async");
 require("dotenv").config();
 
 const app = express()
@@ -8,6 +10,20 @@ const app = express()
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", function (req, res) {
+    res.render("home", {});
+})
+
+app.get("/login", function (req, res) {
+    res.render("login", {});
+})
+
+app.get("/register", function (req, res) {
+    res.render("register", {});
+})
+
 
 
 app.listen(process.env.PORT || 3000, function () {
