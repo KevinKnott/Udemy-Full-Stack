2cc5b2a092609aea44b2a917cebc90a39853d687
Kevin Knott <KKnoTT93@gmail.com>
2020-08-16 18:42:46 -0500

    Added mongoose connection schema and model for user login

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const { findSeries } = require("async");
 require("dotenv").config();
 
 const app = express()
@@ -11,6 +10,42 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Connnect to mongoose 
+const uri =
+    'mongodb+srv://' +
+    process.env.ATLAS_USER +
+    ':' +
+    process.env.ATLAS_PASS +
+    '@first-cluster.r9nhd.mongodb.net/' +
+    process.env.ATLAS_DB +
+    '?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}, function (err) {
+    if (err) {
+        console.log("Unable to connect to DB", err);
+    } else {
+        console.log("Connected to DB");
+    }
+})
+
+// Create a Schema for Users (Email and PASS)
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+// Create a model to use
+const User = mongoose.model("User", userSchema)
 
 app.get("/", function (req, res) {
     res.render("home", {});
